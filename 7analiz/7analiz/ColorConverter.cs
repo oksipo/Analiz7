586e6b7c20252692134949554f7d8dd427eeef9e
NastyaPolianska <nastyapolianska1998@gmail.com>
2017-12-20 12:35:57 +0200

    уыпврнекуыва

---
+++ 7analiz/7analiz/ColorConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace _7analiz
+{
+    public class ColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Color.FromArgb(255, 100, 100, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
