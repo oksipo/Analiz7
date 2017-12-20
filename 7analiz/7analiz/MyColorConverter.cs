40d4c0f85025816b2044a998241c69da3def701d
NastyaPolianska <nastyapolianska1998@gmail.com>
2017-12-20 13:26:49 +0200

    прап

--- 7analiz/7analiz/MyColorConverter.cs
+++ 7analiz/7analiz/MyColorConverter.cs
@@ -13,7 +13,34 @@ namespace _7analiz
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return new SolidColorBrush(Color.FromArgb(255, 100, 100, 0));
+            string temp = (string) value;
+
+            if (temp == "Дуже високою")
+            {
+                return new SolidColorBrush(Color.FromArgb(200, 255, 50, 50));
+            }
+
+            if (temp == "Високою")
+            {
+                return new SolidColorBrush(Color.FromArgb(200, 255, 100, 100));
+            }
+
+            if (temp == "Середньою")
+            {
+                return new SolidColorBrush(Color.FromArgb(200, 255, 255, 100));
+            }
+
+            if (temp == "Низькою")
+            {
+                return new SolidColorBrush(Color.FromArgb(200, 100, 255, 100));
+            }
+
+            if (temp == "Дуже низькою")
+            {
+                return new SolidColorBrush(Color.FromArgb(200, 50, 255, 50));
+            }
+
+            return new SolidColorBrush(Color.FromArgb(0, 0, 0, 0));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
